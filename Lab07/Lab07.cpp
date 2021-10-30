00149b90612c942aa0f2362e4a6f6ebce6eac160
JacobHornbeck <hor20009@byui.edu>
2021-10-29 21:36:04 -0600

    Fill out header stuff

--- Lab07/Lab07.cpp
+++ Lab07/Lab07.cpp
@@ -6,9 +6,13 @@
  * 3. Assignment Description:
  *      Simulate a projectile landing
  * 4. What was the hardest part? Be as specific as possible.
- *
+ *      I think the hardest parts were writing a function to
+ *      find the values from the tables to use with linear
+ *      interpolation and getting the values for the example
+ *      to be closer to the output of the example.
  * 5. How long did it take for you to complete the assignment?
- *
+ *      We worked on this lab 2 different days for about 2 hours
+ *      each day, and then I (Jacob) worked on it about 5 hours.
  *****************************************************************/
 
 #include "Projectile.h"
